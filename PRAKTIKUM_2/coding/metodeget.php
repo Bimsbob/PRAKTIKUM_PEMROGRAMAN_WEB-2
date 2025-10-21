1d704eb5e12d068798df3f4f337f1281b4bd14cb
Ahmad bima <fadholah39@gmail.com>
2025-10-21 09:04:44 +0700

    Add PHP form handling and file upload examples
    Added several PHP files demonstrating form handling with GET and POST methods, file upload functionality, and a student registration system that saves data to a text file. Also included are example forms for guestbook entries, login, and value comparison, along with supporting processing scripts and a sample data file.

---
+++ PRAKTIKUM_2/coding/metodeget.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title> Form method GET </title>
+</head>
+<body>
+<h1>Isi from di bawah</h1>
+<form action="methodgetproses.php" method="get">
+Masukkan nama : <input type"text" name="nama" size="25">
+<input type="submit" value="Proses">
+</form>
+</body>
+</html>
